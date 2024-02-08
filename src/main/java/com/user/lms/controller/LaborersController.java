d87c41ba3c1957d880d93fee2d694708e50c760f
Khantil <kachhiakhantil1999@gmail.com>
2024-02-08 13:57:17 +0530

    Worked on the labor

--- src/main/java/com/user/lms/controller/LaborersController.java
+++ src/main/java/com/user/lms/controller/LaborersController.java
@@ -1,24 +1,42 @@
 package com.user.lms.controller;
 
 import com.user.lms.domain.LaborersService;
+import com.user.lms.entity.User;
+import com.user.lms.models.LaborerModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class LaborersController {
     @Autowired
     private LaborersService laborersService;
 
-    @PostMapping("/deleteLaborer")
-    private String deleteLaborer(@RequestParam Long id){
+    @DeleteMapping("/labour")
+    private String deleteLaborer(@RequestParam("userId") String userId){
         try{
-            this.laborersService.deleteUser(id);
+            this.laborersService.deleteUser(Long.parseLong(userId));
             return "Delete Successfully!!";
         }
         catch(Exception e){
             return "Delete failed:"+e.getMessage();
         }
     }
+
+    @PostMapping("/labour")
+    public String addLabour(@RequestBody LaborerModel laborer){
+        this.laborersService.addLabour(laborer);
+        return "Added Successfully";
+    }
+
+    @GetMapping("/labour")
+    public LaborerModel getLabour(@RequestParam("userId") String userId){
+        return this.laborersService.getLabourById(userId);
+    }
+
+    @PutMapping("/labour")
+    public String editLabour(@RequestBody LaborerModel laborer){
+       return this.laborersService.editLabour(laborer);
+    }
+
 }
--- src/main/java/com/user/lms/repository/UserRoleRepository.java
+++ src/main/java/com/user/lms/repository/UserRoleRepository.java
@@ -3,6 +3,12 @@ package com.user.lms.repository;
 import com.user.lms.entity.Role;
 import com.user.lms.entity.UserRoles;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRoleRepository extends JpaRepository<UserRoles, Long> {
+
+    @Modifying
+    @Query(value = "DELETE FROM user_roles where user_id = ?",nativeQuery = true)
+    void deleteUserRolesByUserId(Long userId);
 }
